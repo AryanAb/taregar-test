ad5731ca552ab447f328298b1c851064278dbf39
Aryan <aryanabed48@gmail.com>
2025-10-21 23:46:46 -0400

    log append

--- change.py
+++ change.py
@@ -5,5 +5,5 @@ subprocess.run(["git", "pull"])
 with open("./change.log", "a") as file:
     file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 subprocess.run(["git", "add", "*"])
-subprocess.run(["git", "commit", "-m", "\"log append\""])
+subprocess.run(["git", "commit", "-m", "log append"])
 subprocess.run(["git", "push"])
